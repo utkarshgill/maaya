f5355ba56bde9982e4e1e90b3406d00490a3a155
Utkarsh Gill <engelbart@Utkarshs-MacBook-Pro.local>
2024-05-07 08:27:26 +0530

    bug fixes

--- engine.py
+++ engine.py
@@ -184,3 +184,9 @@ class Renderer:
                              interval=10, blit=True)
         plt.show()
 
+
+# Example usage:
+world = World(g=10)
+quad = PhysicsObject(position=Vector3D(0, 0, 10.0), mass=1.0)
+world.add_object(quad) 
+world.simulate(1000, render=True)
--- examples/quad_hover.py
+++ examples/quad_hover.py
@@ -1,7 +1,1 @@
-from ..engine import Vector3D, World, PhysicsObject
-
-# Example usage:
-world = World(g=10)
-quad = PhysicsObject(position=Vector3D(0, 0, 10.0), mass=1.0)
-world.add_object(quad) 
-world.simulate(1000, render=True)
+# quad sim
